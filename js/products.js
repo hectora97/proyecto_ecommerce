070e616f8445b3f578f6220ccc7b5c80a562ec43
Hector Alvarez <fakalvarez@gmail.com>
2022-09-04 19:22:12 -0300

    Entrega 2 pronta

--- js/products.js
+++ js/products.js
@@ -122,11 +122,39 @@ function sortPrecio(datarray){
         });
     }
 
+    function showCategoriesList(datarray){
+
+      for (const item2 of datarray) {
+          if (((minCount == undefined) || (minCount != undefined && parseInt(item2.cost) >= minCount)) &&
+              ((maxCount == undefined) || (maxCount != undefined && parseInt(item2.cost) <= maxCount))){
+  
+              container.innerHTML += `
+              <div onclick="setCatID(${item2.id})" class="list-group-item list-group-item-action cursor-active">
+                    <div class="row">
+                        <div class="col-3">
+                            <img src="${item2.image}" alt="${item2.description}" class="img-thumbnail">
+                        </div>
+                        <div class="col">
+                            <div class="d-flex w-100 justify-content-between">
+                                <h4 class="mb-1">${item2.name} - ${item2.currency} ${item2.cost} </h4>
+                                <small class="text-muted">${item2.soldCount} artículos</small>
+                            </div>
+                            <p class="mb-1">${item2.description}</p>
+                        </div>
+                    </div>
+                </div>
+              `
+          }
+  
+      }
+  }
+
 
   async function loadData(){
     let response = await fetch(LINK_PRODUCTOS);
     let json = await response.json();
     showData(json.products);
+    
     document.getElementById("sortAsc").addEventListener("click", function(){
       container.innerHTML="";
       sortPrecio(json.products);
@@ -141,6 +169,41 @@ function sortPrecio(datarray){
       container.innerHTML="";
       sortCantVend(json.products);
     });
+
+    document.getElementById("clearRangeFilter").addEventListener("click", function(){
+      container.innerHTML="";
+      document.getElementById("rangeFilterCountMin").value = "";
+      document.getElementById("rangeFilterCountMax").value = "";
+
+      minCount = undefined;
+      maxCount = undefined;
+
+      showCategoriesList(json.products);
+  });
+
+  document.getElementById("rangeFilterCount").addEventListener("click", function(){
+    
+    container.innerHTML="";
+    minCount = document.getElementById("rangeFilterCountMin").value;
+    maxCount = document.getElementById("rangeFilterCountMax").value;
+
+    if ((minCount != undefined) && (minCount != "") && (parseInt(minCount)) >= 0){
+        minCount = parseInt(minCount);
+    }
+    else{
+        minCount = undefined;
+    }
+
+    if ((maxCount != undefined) && (maxCount != "") && (parseInt(maxCount)) >= 0){
+        maxCount = parseInt(maxCount);
+    }
+    else{
+        maxCount = undefined;
+    }
+
+    showCategoriesList(json.products);
+});
+
 
   }
   
