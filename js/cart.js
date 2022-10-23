0af23c1745e15b9c2ab3ec9fc11530cd37c7fc28
Hector Alvarez <fakalvarez@gmail.com>
2022-10-23 00:20:10 -0300

    Envio calculado, falta suma total final

--- js/cart.js
+++ js/cart.js
@@ -1,6 +1,10 @@
 const user = document.getElementById("username");
 const dataStorage = localStorage.getItem("text");
 const cont = document.getElementById("fila");
+const prem = document.getElementById("prem");
+const expres = document.getElementById("expres");
+const stand = document.getElementById("stand");
+
 
     function cargarHTML(){
         user.innerHTML += localStorage.getItem("text") 
@@ -10,10 +14,33 @@ const cont = document.getElementById("fila");
     const sumarT = (costoU) => {
         const cantT = document.getElementById("cant");
         const total = document.getElementById("total");
-        const result = costoU * cantT.value;
+        const cost = document.getElementById("costo");
+        var result = costoU * cantT.value;
         console.log(result);
-        total.innerHTML =`<p>USD${result}</p>`;
-    }
+        total.innerHTML =`<p id="costo1" value=${result}> USD${result} </p>`;
+        cost.innerHTML =`
+            ${result}
+            `
+    };
+
+    const todoCost = (unitcost) => {
+        const costE = document.getElementById("costE");
+        const premI = document.getElementById("premI");
+        const expresI = document.getElementById("expresI");
+        const standI = document.getElementById("standI");
+        const costEnv = parseFloat(
+            premI.checked
+                ? premI.value
+                : expresI.checked
+                ? expresI.value
+                : standI.value
+        );
+        const final = unitcost * (costEnv / 100);
+        costE.innerHTML = `
+            <p id="" value=${final}> USD${final} </p>
+        `
+        console.log(final);
+    };
 
     function mostrarData(arr){
         console.log(arr);
@@ -25,6 +52,25 @@ const cont = document.getElementById("fila");
             <td style="width:20%;"><input type="number" style="width:30%;" id="cant" oninput="sumarT(${item1.unitCost})"></input></td>
             <td id="total"></td>
             ` 
+            prem.innerHTML=`
+            <input class="form-check-input" value="15" type="radio" name="flexRadioDefault" id="premI" checked onclick="todoCost(${item1.unitCost})">
+            <label class="form-check-label" for="flexRadioDefault2">
+                Premium 2 a 5 dias (15%).
+            </label>
+            `
+
+            expres.innerHTML=`
+            <input class="form-check-input" value="7" type="radio" name="flexRadioDefault" id="expresI"  onclick="todoCost(${item1.unitCost})">
+            <label class="form-check-label" for="flexRadioDefault1">
+            Express 5 a 8 dias (7%). 
+            </label>
+            `
+            stand.innerHTML=`
+            <input class="form-check-input" value="5" type="radio" name="flexRadioDefault" id="standI"  onclick="todoCost(${item1.unitCost})">
+            <label class="form-check-label" for="flexRadioDefault1">
+            Standar 12 a 15 dias (5%). 
+            </label>
+            `
         }
         
     }
